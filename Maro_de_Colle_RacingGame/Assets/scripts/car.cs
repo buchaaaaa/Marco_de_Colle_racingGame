489b4eeb7ea4501fd5366b8a4b657fbe12098be0
buchaaaaa <tobias.pucher@htl-wolfsberg.at>
2023-05-05 14:22:44 +0200

    movement

--- Maro_de_Colle_RacingGame/Assets/scripts/car.cs
+++ Maro_de_Colle_RacingGame/Assets/scripts/car.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class car : MonoBehaviour
 {
-
+    public float accelaration = 1;
+    public float steerling = 1;
     Rigidbody rigid;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +20,12 @@ public class car : MonoBehaviour
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
-        rigid.AddForce(transform.forward * verticalInput *10 );
+        rigid.AddForce(transform.forward * verticalInput * 2* accelaration * Time.deltaTime);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * steerling* Time.deltaTime);
+
+        Vector3 velocity = rigid.velocity;
+        velocity = transform.forward * velocity.magnitude;
+        rigid.velocity = velocity;
     }
 }
